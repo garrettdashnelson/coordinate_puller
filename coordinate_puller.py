a507dcf9133bb09cf14e271e9f10370a0fc5ab13
Garrett Nelson <garrettdashnelson@gmail.com>
2013-10-20 20:00:27 -0500

    Much simpler and flexible etree searching

--- coordinate_puller.py
+++ coordinate_puller.py
@@ -2,12 +2,7 @@ from pykml import parser
 
 file = raw_input('Enter the file name: ')
 
-doc = parser.parse(file).getroot()
+doc = parser.parse(file)
 
-for el in doc.Document.Placemark:
-	if el.find("LineString") is not None:
-		for s in el.LineString:
-			print s.coordinates.text
-	if el.find("Point") is not None:
-		for s in el.Point:
-			print s.coordinates.text
+for elt in doc.findall('//coordinates'):
+	print elt.text
